e6ade9e19b90cb894b3e3671969dabff77d7ff93
vimukthi <vimmperera@gmail.com>
2020-05-18 12:14:35 +0530

    Parameterize scripts for TG

--- modules/ob_common/manifests/params.pp
+++ modules/ob_common/manifests/params.pp
@@ -39,7 +39,7 @@ class ob_common::params {
     $jdk_type = "OpenJDK11U-jdk_x64_linux_hotspot_11.0.5_10.tar.gz"
     $jdk_path = "jdk-11.0.5+10"
   }
-  
+
   $java_home    = "${java_dir}/${jdk_name}"
 
   $wso2_service_name  = "wso2${profile}"
@@ -57,64 +57,139 @@ class ob_common::params {
   $server_script_path = "${product_dir}/${pack}/bin/wso2server.sh"
   $pid_file_path      = "${product_dir}/${pack}/wso2carbon.pid"
 
-  # Hostname changes in apimanager.xml, openbanking.xml, carbon.xml, identity.xml, application-authentication.xml, conf.json params and velocity_template.xml params
-  $iam_hostname       = 'localhost'
+  # Hostname changes in apimanager.xml, openbanking.xml, carbon.xml, identity.xml,
+  # application-authentication.xml, conf.json params and velocity_template.xml params
+  $iam_hostname       = 'CF_OBKM_HOSTNAME'
   $analytics_hostname = 'localhost'
-  $apim_hostname      = 'localhost'
+  $apim_hostname      = 'CF_OBAM_HOSTNAME'
   $bps_hostname       = 'localhost'
 
   # jaggeryapps/admin/site/conf/site.json,openbaking.xml params
-  $spec = 'UK' #UK,Berlin or STET
+  $spec = 'CF_DEPLOYED_SPEC' #UK,Berlin or STET
 
   # DBMS related variables
+  $db_password = 'CF_DB_PASSWORD'
+  $db_managment_system = 'CF_DBMS'
+  $oracle_sid = 'WSO2OBDB'
+
+  if $db_managment_system == 'mysql' {
+    $reg_db_user_name = 'CF_DB_USERNAME'
+    $um_db_user_name = 'CF_DB_USERNAME'
+    $am_db_user_name = 'CF_DB_USERNAME'
+    $am_stat_db_user_name = 'CF_DB_USERNAME'
+    $mb_store_db_user_name = 'CF_DB_USERNAME'
+    $am_config_db_user_name = 'CF_DB_USERNAME'
+    $km_config_db_user_name = 'CF_DB_USERNAME'
+    $ob_db_user_name = 'CF_DB_USERNAME'
+
+    $wso2_reg_db_url = 'jdbc:mysql://CF_RDS_URL:3306/openbank_govdb?autoReconnect=true&amp;useSSL=false'
+    $wso2_um_db_url = 'jdbc:mysql://CF_RDS_URL:3306/openbank_userdb?autoReconnect=true&amp;useSSL=false'
+    $wso2_am_db_url = 'jdbc:mysql://CF_RDS_URL:3306/openbank_apimgtdb?autoReconnect=true&amp;useSSL=false'
+    $wso2_am_config_db_url = 'jdbc:mysql://CF_RDS_URL:3306/openbank_am_configdb?autoReconnect=true&amp;useSSL=false'
+    $wso2_km_config_db_url = 'jdbc:mysql://CF_RDS_URL:3306/openbank_iskm_configdb?autoReconnect=true&amp;useSSL=false'
+    $wso2_am_stat_db_url = 'jdbc:mysql://CF_RDS_URL:3306/openbank_apimgt_statsdb?autoReconnect=true&amp;useSSL=false'
+    $wso2_mb_store_db_url = 'jdbc:mysql://CF_RDS_URL:3306/openbank_mbstoredb?autoReconnect=true&amp;useSSL=false'
+    $wso2_ob_db_url = 'jdbc:mysql://CF_RDS_URL:3306/openbank_openbankingdb?autoReconnect=true&amp;useSSL=false'
+
+    $db_driver_class_name = 'com.mysql.jdbc.Driver'
+    $db_connector = 'mysql-connector-java-5.1.41-bin.jar'
+    $db_validation_query = 'SELECT 1'
+
+  } elsif $db_managment_system =~ 'oracle' {
+    $reg_db_user_name = 'openbank_govdb'
+    $um_db_user_name = 'openbank_userdb'
+    $am_db_user_name = 'openbank_apimgtdb'
+    $am_stat_db_user_name = 'openbank_apimgt_statsdb'
+    $mb_store_db_user_name = 'openbank_mbstoredb'
+    $am_config_db_user_name = 'openbank_am_configdb'
+    $km_config_db_user_name = 'openbank_iskm_configdb'
+    $ob_db_user_name = 'openbank_openbankingdb'
+
+    $wso2_reg_db_url = "jdbc:oracle:thin:@CF_RDS_URL:1521/${oracle_sid}"
+    $wso2_um_db_url = "jdbc:oracle:thin:@CF_RDS_URL:1521/${oracle_sid}"
+    $wso2_am_db_url = "jdbc:oracle:thin:@CF_RDS_URL:1521/${oracle_sid}"
+    $wso2_am_config_db_url = "jdbc:oracle:thin:@CF_RDS_URL:1521/${oracle_sid}"
+    $wso2_km_config_db_url = "jdbc:oracle:thin:@CF_RDS_URL:1521/${oracle_sid}"
+    $wso2_am_stat_db_url = "jdbc:oracle:thin:@CF_RDS_URL:1521/${oracle_sid}"
+    $wso2_mb_store_db_url = "jdbc:oracle:thin:@CF_RDS_URL:1521/${oracle_sid}"
+    $wso2_ob_db_url = "jdbc:oracle:thin:@CF_RDS_URL:1521/${oracle_sid}"
+
+    $db_driver_class_name = 'oracle.jdbc.OracleDriver'
+    $db_validation_query = 'SELECT 1 FROM DUAL'
+    $db_connector = 'ojdbc8.jar'
+
+  } elsif $db_managment_system == 'sqlserver-se' {
+    $reg_db_user_name = 'CF_DB_USERNAME'
+    $um_db_user_name = 'CF_DB_USERNAME'
+    $am_db_user_name = 'CF_DB_USERNAME'
+    $am_stat_db_user_name = 'CF_DB_USERNAME'
+    $mb_store_db_user_name = 'CF_DB_USERNAME'
+    $am_config_db_user_name = 'CF_DB_USERNAME'
+    $km_config_db_user_name = 'CF_DB_USERNAME'
+    $ob_db_user_name = 'CF_DB_USERNAME'
+
+    $wso2_reg_db_url = 'jdbc:sqlserver://CF_RDS_URL:1433;databaseName=openbank_govdb;SendStringParametersAsUnicode=false'
+    $wso2_um_db_url = 'jdbc:sqlserver://CF_RDS_URL:1433;databaseName=openbank_userdb;SendStringParametersAsUnicode=false'
+    $wso2_am_db_url = 'jdbc:sqlserver://CF_RDS_URL:1433;databaseName=openbank_apimgtdb;SendStringParametersAsUnicode=false'
+    $wso2_am_config_db_url = 'jdbc:sqlserver://CF_RDS_URL:1433;databaseName=openbank_am_configdb;SendStringParametersAsUnicode=false'
+    $wso2_km_config_db_url = 'jdbc:sqlserver://CF_RDS_URL:1433;databaseName=openbank_iskm_configdb;SendStringParametersAsUnicode=false'
+    $wso2_am_stat_db_url = 'jdbc:sqlserver://CF_RDS_URL:1433;databaseName=openbank_apimgt_statsdb;SendStringParametersAsUnicode=false'
+    $wso2_mb_store_db_url = 'jdbc:sqlserver://CF_RDS_URL:1433;databaseName=openbank_mbstoredb;SendStringParametersAsUnicode=false'
+    $wso2_ob_db_url = 'jdbc:sqlserver://CF_RDS_URL:1433;databaseName=openbank_openbankingdb;SendStringParametersAsUnicode=false'
+
+    $db_driver_class_name = 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
+    $db_connector = 'mssql-jdbc-7.0.0.jre8.jar'
+    $db_validation_query = 'SELECT 1'
+
+  }
 
   # master-datasources.xml params
-  $db_apimgt_url              = 'jdbc:h2:repository/database/WSO2AM_DB;DB_CLOSE_ON_EXIT=FALSE'
-  $db_apimgt_username         = 'wso2carbon'
-  $db_apimgt_password         = 'wso2carbon'
-  $db_apimgt_validation_query = 'SELECT 1'
-  $db_apimgt_driver           = 'org.h2.Driver'
+  $db_apimgt_url              = $wso2_am_db_url
+  $db_apimgt_username         = $am_db_user_name
+  $db_apimgt_password         = $db_password
+  $db_apimgt_validation_query = $db_validation_query
+  $db_apimgt_driver           = $db_driver_class_name
 
-  $db_mb_store_url              = 'jdbc:h2:repository/database/WSO2MB_DB;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000'
-  $db_mb_store_username         = 'wso2carbon'
-  $db_mb_store_password         = 'wso2carbon'
-  $db_mb_store_validation_query = 'SELECT 1'
-  $db_mb_store_driver           = 'org.h2.Driver'
+  $db_mb_store_url              = $wso2_mb_store_db_url
+  $db_mb_store_username         = $mb_store_db_user_name
+  $db_mb_store_password         = $db_password
+  $db_mb_store_validation_query = $db_validation_query
+  $db_mb_store_driver           = $db_driver_class_name
 
-  $db_apimgt_stat_url               = 'jdbc:h2:repository/database/WSO2_APIMGT_STAT_DB;DB_CLOSE_ON_EXIT=FALSE'
-  $db_apimgt_stat_username          = 'wso2carbon'
-  $db_apimgt_stat_password          = 'wso2carbon'
-  $db_apimgt_stat_validation_query  = 'SELECT 1'
-  $db_apimgt_stat_driver            = 'org.h2.Driver'
+  $db_apimgt_stat_url               = $wso2_am_stat_db_url
+  $db_apimgt_stat_username          = $am_stat_db_user_name
+  $db_apimgt_stat_password          = $db_password
+  $db_apimgt_stat_validation_query  = $db_validation_query
+  $db_apimgt_stat_driver            = $db_driver_class_name
 
-  $db_am_config_url                 = 'jdbc:h2:repository/database/WSO2_AM_CONFIG_DB;DB_CLOSE_ON_EXIT=FALSE'
-  $db_am_config_username            = 'wso2carbon'
-  $db_am_config_password            = 'wso2carbon'
-  $db_am_config_validation_query    = 'SELECT 1'
-  $db_am_config_driver              = 'org.h2.Driver'
+  $db_am_config_url                 = $wso2_am_config_db_url
+  $db_am_config_username            = $am_config_db_user_name
+  $db_am_config_password            = $db_password
+  $db_am_config_validation_query    = $db_validation_query
+  $db_am_config_driver              = $db_driver_class_name
 
-  $db_km_config_url               = 'jdbc:h2:repository/database/WSO2Config_DB;DB_CLOSE_ON_EXIT=FALSE'
-  $db_km_config_username          = 'wso2carbon'
-  $db_km_config_password          = 'wso2carbon'
-  $db_km_config_validation_query  = 'SELECT 1'
-  $db_km_config_driver            = 'org.h2.Driver'
+  $db_km_config_url               = $wso2_km_config_db_url
+  $db_km_config_username          = $km_config_db_user_name
+  $db_km_config_password          = $db_password
+  $db_km_config_validation_query  = $db_validation_query
+  $db_km_config_driver            = $db_driver_class_name
 
-  $db_gov_url                       = 'jdbc:h2:repository/database/WSO2REG_DB;DB_CLOSE_ON_EXIT=FALSE'
-  $db_gov_username                  = 'wso2carbon'
-  $db_gov_password                  = 'wso2carbon'
-  $db_gov_validation_query          = 'SELECT 1'
-  $db_gov_driver                    = 'org.h2.Driver'
+  $db_gov_url                       = $wso2_reg_db_url
+  $db_gov_username                  = $reg_db_user_name
+  $db_gov_password                  = $db_password
+  $db_gov_validation_query          = $db_validation_query
+  $db_gov_driver                    = $db_driver_class_name
 
-  $db_user_store_url                = 'jdbc:h2:repository/database/WSO2UM_DB;DB_CLOSE_ON_EXIT=FALSE'
-  $db_user_store_username           = 'wso2carbon'
-  $db_user_store_password           = 'wso2carbon'
-  $db_user_store_validation_query   = 'SELECT 1'
-  $db_user_store_driver             = 'org.h2.Driver'
+  $db_user_store_url                = $wso2_um_db_url
+  $db_user_store_username           = $um_db_user_name
+  $db_user_store_password           = $db_password
+  $db_user_store_validation_query   = $db_validation_query
+  $db_user_store_driver             = $db_driver_class_name
 
   # open-banking-datasources.xml params
-  $db_open_banking_store_url              = 'jdbc:h2:repository/database/WSO2_OPEN_BANKING_STORE_DB;DB_CLOSE_ON_EXIT=FALSE'
-  $db_open_banking_store_username         = 'wso2carbon'
-  $db_open_banking_store_password         = 'wso2carbon'
-  $db_open_banking_store_validation_query = 'SELECT 1'
-  $db_open_banking_store_driver           = 'org.h2.Driver'
+  $db_open_banking_store_url              = $wso2_ob_db_url
+  $db_open_banking_store_username         = $ob_db_user_name
+  $db_open_banking_store_password         = $db_password
+  $db_open_banking_store_validation_query = $db_validation_query
+  $db_open_banking_store_driver           = $db_driver_class_name
 }
